98f93ff00cf7c0b4845d8d18b4def9dd129b4c1c
sahilchandla <sahil017kumar@gmail.com>
2026-08-15 21:22:15 +0530

    Show Render port in server log

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,6 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log("server run");
+    console.log(`Server running on port ${PORT}`);
 });
 
