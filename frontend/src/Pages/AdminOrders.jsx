2b4c0963d093f7b3870984b31590bc1cb5f0d41f
sahilchandla <sahil017kumar@gmail.com>
2026-08-16 15:33:34 +0530

    Fix production orders API URL

--- frontend/src/Pages/AdminOrders.jsx
+++ frontend/src/Pages/AdminOrders.jsx
@@ -13,7 +13,7 @@ function AdminOrders() {
 
             // console.log("Token 👉", token);
 
-            const res = await axios.get("http://localhost:5000/api/orders", {
+            const res = await axios.get("https://chandla-steel-works-1.onrender.com/api/orders", {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 },
@@ -40,7 +40,7 @@ function AdminOrders() {
             const token = JSON.parse(localStorage.getItem("token"));
 
             await axios.put(
-                `http://localhost:5000/api/orders/update/${id}`,
+                `https://chandla-steel-works-1.onrender.com/api/orders/update/${id}`,
                 { status },
                 {
                     headers: {
